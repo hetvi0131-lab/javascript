94bd37e5dcb6a4be6549f85cdcbfd9f748681401
hetvi0131-lab <hetvi0131@gmail.com>
2026-02-05 13:39:49 +0530

    msg

---
+++ object.js
@@ -0,0 +1,16 @@
+// Objects vs Array
+// Objects -- when you want to show all data based on
+// one entity(k j entity mate all data jota hoy) (ex.
+// one user data)
+
+// Array -- when you want to show entity (ex. all
+// users)
+
+// create
+// varabile = {}
+// key : value --> key-value sturcture
+let User ={
+    name:"Test",
+    email:"test@gmail.com",
+    rollno:7
+}
